510755753d8cd82e3e6e7e944325fd978720b1c9
Salmaan <salmaan.arif.ali@gmail.com>
2021-02-17 23:17:02 -0500

    commit tree fec3582148633e240058e404077a4411a5109ec1

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,33 @@
 class Pokemon
+  attr_accessor :id, :name, :type, :db
+
+  def initialize(hash)
+    hash.each do |key, value|
+      self.send("#{key}=", value)
+    end
+  end
+
+  def self.save(name, type, db)
+    sql = <<-SQL
+      INSERT INTO pokemon (name, type)
+      VALUES (?, ?)
+      SQL
+    db.execute(sql, name, type)
+  end
+
+  def self.find(id, db)
+    sql = <<-SQL
+      SELECT *
+      FROM pokemon
+      WHERE id = ?
+      SQL
+
+    row = db.execute(sql, id)[0]
+
+    new_pokemon = self.new({})
+    new_pokemon.id = row[0]
+    new_pokemon.name = row[1]
+    new_pokemon.type = row[2]
+    new_pokemon
+  end
 end
